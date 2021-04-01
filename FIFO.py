9554596124297a0f5ced111b97953afbf5e0205e
brasfx <foxpc18@gmail.com>
2021-04-01 15:49:49 -0300

    Implementacao FIFO

--- FIFO.py
+++ FIFO.py
@@ -1,3 +1,15 @@
-def FIFO(moldura, processo, paginas):
-    print("teste")
+def FIFO(count_moldura, processos, paginas):
+    miss = 0
 
+    molduras = []
+
+    for pag in paginas:
+        if pag not in molduras:
+            miss += 1
+
+            if len(molduras) == count_moldura:
+                molduras.pop(0)
+
+            molduras.append(pag)
+
+    return miss
