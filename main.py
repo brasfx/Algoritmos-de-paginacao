502289bfbd3ad299b1457f6dade079faa4ae888f
Renan Paredes Barreto <Renan.barretoo@hotmail.com>
2021-04-01 14:38:56 -0300

    Separação de arquivos

--- main.py
+++ main.py
@@ -2,24 +2,9 @@ from random import randint
 from FIFO import FIFO
 from MRU import MRU
 from NUF import NUF
-
-def readfile():  # retorna uma matriz de strings para representar o arquivo de processos
-    result = []
-    #nome_do_arquivo = input('Digite o nome do arquivo:\n')
-    proc = open('entrada.txt', 'r')
-    proc = (proc.read()).splitlines()
-    for line in proc:
-        line = line.split("|")
-        result.append(line)
-    return (result)
+from tratamento import tratamento,readfile
 
 
-def tratamento(arquivo):
-    for element in arquivo:  # converte as strings de numeros da lista em inteiros
-        for it in range(0, 3):
-            element[it] = element[it].replace(' ', ',')
-            element[it] = eval(element[it])
-    return arquivo
 
 
 
---
+++ tratamento.py
@@ -0,0 +1,17 @@
+def readfile():  # retorna uma matriz de strings para representar o arquivo de processos
+    result = []
+    #nome_do_arquivo = input('Digite o nome do arquivo:\n')
+    proc = open('entrada.txt', 'r')
+    proc = (proc.read()).splitlines()
+    for line in proc:
+        line = line.split("|")
+        result.append(line)
+    return (result)
+
+
+def tratamento(arquivo):
+    for element in arquivo:  # converte as strings de numeros da lista em inteiros
+        for it in range(0, 3):
+            element[it] = element[it].replace(' ', ',')
+            element[it] = eval(element[it])
+    return arquivo
