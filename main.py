f9c77482130d6485f0e399c75c89189de38c0e1e
Renan Paredes Barreto <Renan.barretoo@hotmail.com>
2021-04-01 16:16:08 -0300

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,19 @@ from tratamento import tratamento, readfile
 
 
 saida = tratamento(readfile())
+nfu = 1000
+otimo = "meh"
 for line in saida:
-    print(FIFO(line[0],line[1],line[2]),"|",MRU(line[0],line[1],line[2]))
+    fifo = FIFO(line[0],line[1],line[2])
+    mru = MRU(line[0],line[1],line[2])
+   # nfu = NUF(line[0],line[1],line[2])
+    if fifo < mru <= nfu:
+        menor = "fifo"
+    elif mru < fifo <= nfu:
+        menor = "mru"
+    elif nfu < fifo <= mru:
+        menor = "nfu"
+    else:
+        menor = "empate"
+    print(fifo,"|",mru,"|",nfu,"|",otimo,"|",menor)
 
